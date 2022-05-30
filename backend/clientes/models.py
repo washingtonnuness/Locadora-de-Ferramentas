cd1ff9f4dab9bdb54e95b3e3d29c132e82a68dce
Regis Santos <regis42santos@gmail.com>
2022-05-30 00:58:15 -0300

    Reset de migrações

--- backend/clientes/models.py
+++ backend/clientes/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Clientes(models.Model):
     CNPJ = 'CNPJ'
     CPF = 'CPF'
-    
+
     TIPO_REGISTRO = [
         (CNPJ, CNPJ),
         (CPF, CPF),
@@ -29,17 +29,19 @@ class Clientes(models.Model):
     cidade = models.CharField("Cidade", max_length=100)
     estado = models.CharField("Estado", max_length=100)
     cep = models.CharField("Cep", max_length=100)
-    delete_user = models.CharField(max_length=100, null=True, blank=True) 
+    delete_user = models.CharField(max_length=100, null=True, blank=True)
     checkbox = models.BooleanField("Desativar", max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     delete_at = models.DateTimeField(auto_now=False, null=True)
-    
+
     def is_upperclass(self):
         return self.tipo in {self.CNPJ, self.CPF}
-
 
     class Meta:
         verbose_name = "Cliente"
         verbose_name_plural = "Cliente"+'s'
         ordering = ('tipo', 'nome', 'nomeFantasia')
+
+    def __str__(self):
+        return f'{self.nome}'
--- backend/orcamentos/admin.py
+++ backend/orcamentos/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Orcamentos, OrcamentosItens
 
-# Register your models here.
+
+class OrcamentosItensInline(admin.TabularInline):
+    model = OrcamentosItens
+    extra = 0
+
+
+@admin.register(Orcamentos)
+class OrcamentosAdmin(admin.ModelAdmin):
+    inlines = (OrcamentosItensInline,)
+    list_display = ('__str__',)
+    # readonly_fields = ('slug',)
+    # list_display_links = ('name',)
+    # search_fields = ('name',)
+    # list_filter = ('type',)
+    # date_hierarchy = 'created'
+    # ordering = ('-created',)
+    # actions = ('',)
