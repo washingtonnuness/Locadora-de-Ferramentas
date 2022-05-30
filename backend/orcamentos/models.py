e15713e75fbbf9af33f75acd377f2e1e66e7a9e6
Regis Santos <regis42santos@gmail.com>
2022-05-30 00:57:19 -0300

    Adiciona modelagem OrcamentosItens

--- backend/orcamentos/models.py
+++ backend/orcamentos/models.py
@@ -5,28 +5,20 @@ from backend.produtos.models import Produtos, Patrimonio
 
 
 class Orcamentos(models.Model):
-    cliente = models.ManyToManyField(
+    cliente = models.ForeignKey(
         Clientes,
+        on_delete=models.SET_NULL,
+        related_name='cliente_orcamentos',
+        null=True,
         blank=True
     )
-
-    produtos = models.ManyToManyField(
-        Produtos,
-        blank=True
-    )
-    patrimonio = models.ManyToManyField(
-        Patrimonio,
-        blank=True
-    )
-    valor = models.DecimalField('Valor', max_digits=6, decimal_places=2, blank=True, null=True)
-
     created_user = models.CharField(max_length=100, null=True, blank=True)
     delete_user = models.CharField(max_length=100, null=True, blank=True)
 
     checkbox = models.BooleanField("Excluir", max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    delete_at = models.DateTimeField(auto_now=False, null=True)
+    delete_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         ordering = ('created_at',)
@@ -34,4 +26,41 @@ class Orcamentos(models.Model):
         verbose_name_plural = "Orçamentos"
 
     def __str__(self):
-        return f'{self.pk} - {self.produtos.nome} - {self.patrimonio}'
+        return f'{self.pk}'
+
+
+class OrcamentosItens(models.Model):
+    orcamento = models.ForeignKey(
+        Orcamentos,
+        on_delete=models.CASCADE,
+        related_name='orcamentos',
+    )
+    produto = models.ForeignKey(
+        Produtos,
+        on_delete=models.SET_NULL,
+        related_name='produto_orcamentos',
+        null=True,
+        blank=True
+    )
+    patrimonio = models.ForeignKey(
+        Patrimonio,
+        on_delete=models.SET_NULL,
+        related_name='patrimonio_orcamentos',
+        null=True,
+        blank=True
+    )
+    valor = models.DecimalField(
+        'Valor',
+        max_digits=6,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = "Orçamento Item"
+        verbose_name_plural = "Orçamento Itens"
+
+    def __str__(self):
+        return f'{self.pk}'
