882e61f47ad556871cfa2e02cb95a90208eca219
rg3915 <regis42santos@gmail.com>
2022-07-05 08:35:37 -0300

    Arruma menu. close #16

--- backend/orcamento/urls.py
+++ backend/orcamento/urls.py
@@ -6,8 +6,8 @@ app_name = 'orcamento'
 
 
 urlpatterns = [
-    path('create/<int:client_pk>', v.orcamento_create, name='orcamento_create'),
     path('list/', v.orcamento_list, name='orcamento_list'),
+    path('create/', v.orcamento_create, name='orcamento_create'),
     path('search/', v.search, name='search'),
     path('add-row/<int:pk>', v.add_row_hx, name='add_row_hx'),
 ]
