d82edf06559510ccf422250491e6c71399ac0dfa
rg3915 <regis42santos@gmail.com>
2022-07-05 09:29:57 -0300

    Corrige login csrf_token em produção. close #19

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
-CSRF_TRUSTED_ORIGINS = ['https://locatools.com.br/']
+CSRF_TRUSTED_ORIGINS = ['https://locatools.com.br',
+                        'https://locatools.herokuapp.com/']
 
 
 # Application definition
