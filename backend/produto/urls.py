442b17cd55655cf208c475ff086751dd7f23710c
washingtonnuness <washingtonnuness@gmail.com>
2022-07-06 17:56:36 -0300

    Sistema de busca
    Aplicado sistema de busca de itens.
    Falta terminar.

--- backend/produto/urls.py
+++ backend/produto/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', v.ProdutoListView.as_view(), name='produto_list'),
     path('create/', v.produto_create, name='produto_create'),
     path('add-row/', v.add_row_produto_items_hx, name='add_row_produto_items_hx'),  # noqa E501
+    path('produto-search/', v.produto_items_search, name='produto_items_search'), 
 ]
--- backend/produto/views.py
+++ backend/produto/views.py
@@ -31,7 +31,7 @@ def produto_create(request):
 def add_row_produto_items_hx(request):
     template_name = 'produto/hx/row_produto_items_hx.html'
     form = PatrimonioForm()
-    context = {'produto_item_form': form}
+    context = {'produto_item_row_form': form}
     return render(request, template_name, context)
 
 
@@ -39,3 +39,10 @@ def order_item_delete(request, pk):
     order_item = Produto.objects.get(pk=pk)
     order_item.delete()
     return HttpResponse('')
+
+def produto_items_search(request):
+    template = 'produto/hx/search-results.html'
+    search_field = request.GET.get('search')
+    results = Produto.objects.filter(titulo__icontains=search_field)
+    context = {"results": results}
+    return render(request, template, context)
