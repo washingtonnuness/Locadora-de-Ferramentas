626f03f43177719afd26dc57b45208362e3bc2cd
rg3915 <regis42santos@gmail.com>
2022-07-08 10:12:52 -0300

    Atualiza requirements e tira client_pk da url de criar orçamento.

--- backend/orcamento/urls.py
+++ backend/orcamento/urls.py
@@ -7,7 +7,7 @@ app_name = 'orcamento'
 
 urlpatterns = [
     path('list/', v.OrcamentoListView.as_view(), name='orcamento_list'),
-    path('create/<int:client_pk>', v.orcamento_create, name='orcamento_create'),
+    path('create/', v.orcamento_create, name='orcamento_create'),
     path('search/', v.search, name='search'),
     path('add-row/<int:pk>', v.add_row_hx, name='add_row_hx'),
 ]
